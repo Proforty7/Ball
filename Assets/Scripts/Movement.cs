d77a0979babcf6c574fde9636bbe48ee43900632
Proforty7 <33191256+Proforty7@users.noreply.github.com>
2017-11-10 16:47:16 +0530

    Added real movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,21 +5,42 @@ using UnityEngine;
 public class Movement : MonoBehaviour {
 
     public Material bg;
+    private Transform playerTransform;
     float k;
-	// Use this for initialization
-	void Start () {
+    public float factor;
+    Vector2 origin, target;
+
+    void Awake()
+    {
+        playerTransform = GameObject.Find("Player").GetComponent<Transform>();
+    }
+    // Use this for initialization
+    void Start () {
         bg.mainTextureOffset = new Vector2(bg.mainTextureOffset.x, 0);
         k = 0.005f;
         InvokeRepeating("incSpeed", 5, 5);
     }
 	
 	// Update is called once per frame
-	void Update () {
-        
-		if(Input.GetButton("Fire1"))
+	void Update ()
+    {
+
+        Vector2 newPosition;
+                
+        if(Input.GetButtonDown("Fire1"))
         {
-            Debug.Log(k);
+            origin = Input.mousePosition;
+            //origin = Camera.main.ScreenToWorldPoint(target);
+        }
+		else if(Input.GetButton("Fire1"))
+        {
+           
+            newPosition = Input.mousePosition;
+            float pos = newPosition.x - origin.x;
+            Debug.Log(origin + " " + newPosition + " " + pos);
+            playerTransform.transform.position += new Vector3(pos * factor, 0, 0);
             bg.mainTextureOffset = new Vector2(bg.mainTextureOffset.x, bg.mainTextureOffset.y + k);
+            
         }
 	}
 
