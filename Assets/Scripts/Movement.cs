fa1713aa727dc6ba2f83ea3db88bdaa77a825ce1
Proforty7 <33191256+Proforty7@users.noreply.github.com>
2017-11-11 18:13:57 +0530

    Improved movement. No need to set factor now @prashant-123

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -26,23 +26,41 @@ public class Movement : MonoBehaviour {
     {
 
         Vector2 newPosition;
-                
-        if(Input.GetButtonDown("Fire1"))
+
+         /*  if(Input.GetButtonDown("Fire1"))
+           {
+               target = Input.mousePosition;
+               origin = Camera.main.ScreenToWorldPoint(target);
+           }/*
+           else if(Input.GetButton("Fire1"))
+           {
+
+               newPosition = Input.mousePosition;
+               float pos = newPosition.x - origin.x;
+               Debug.Log(origin + " " + newPosition + " " + pos);
+               playerTransform.transform.position += new Vector3(pos * factor, 0, 0);
+               bg.mainTextureOffset = new Vector2(bg.mainTextureOffset.x, bg.mainTextureOffset.y + k);
+
+           }*/
+
+        if (Input.GetButton("Fire1"))
         {
-            origin = Input.mousePosition;
-            //origin = Camera.main.ScreenToWorldPoint(target);
-        }
-		else if(Input.GetButton("Fire1"))
-        {
-           
+       
             newPosition = Input.mousePosition;
-            float pos = newPosition.x - origin.x;
-            Debug.Log(origin + " " + newPosition + " " + pos);
-            playerTransform.transform.position += new Vector3(pos * factor, 0, 0);
+            Vector2 pos = Camera.main.ScreenToWorldPoint(newPosition);
+            Debug.Log(pos);
+           // float final = newPosition.x - origin.x;
+          //  Debug.Log(origin + " " + newPosition + " " + pos);
+          //  while(playerTransform.transform.position.x != pos.x)
+                playerTransform.transform.position = new Vector3(pos.x * factor, -2.45f, -2);
             bg.mainTextureOffset = new Vector2(bg.mainTextureOffset.x, bg.mainTextureOffset.y + k);
-            
+
+
         }
-	}
+
+        else
+
+    }
 
     private void incSpeed()
     {
